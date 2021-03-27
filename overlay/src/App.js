570f969e3122385ddf091dd9c6ce72931aaee1ac
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-03-27 15:23:49 +0530

    created react app for overlay

---
+++ overlay/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Overlay from "./components/overlay/Overlay";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/:id/:session">
+            <Overlay />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
