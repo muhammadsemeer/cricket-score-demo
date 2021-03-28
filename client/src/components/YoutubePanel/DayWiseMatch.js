65d311334e6e73ff4b4895f4d3be9caf6cab2207
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-03-28 13:33:23 +0530

    day wise and stas

---
+++ client/src/components/YoutubePanel/DayWiseMatch.js
@@ -0,0 +1,21 @@
+import { Grid, Button, ButtonGroup } from "@material-ui/core";
+import { useStyles } from "./panelStyles";
+
+export const DayWiseMatch = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Grid item xs={12} className={classes.gridItems}>
+        <Button variant="conatined" className={classes.infoBtn} disabled>
+          Day Wise Matches List
+        </Button>
+        <ButtonGroup
+          className={classes.btnGrp}
+          style={{ justifyContent: "center" }}
+        >
+          <Button variant="contained">Day 1</Button>
+        </ButtonGroup>
+      </Grid>
+    </>
+  );
+};
---
+++ client/src/components/YoutubePanel/Stat.js
@@ -0,0 +1,34 @@
+import { Button, ButtonGroup, Grid } from "@material-ui/core";
+import { useStyles } from "./panelStyles";
+
+export const Stat = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Grid item xs={12} className={classes.gridItems}>
+        <Button variant="conatined" className={classes.infoBtn} disabled>
+          Statistics
+        </Button>
+        <ButtonGroup
+          className={classes.btnGrp}
+          style={{ justifyContent: "center" }}
+        >
+          <Button variant="contained">Most Runs</Button>
+          <Button variant="contained">Most Wickets</Button>
+          <Button variant="contained">Most 6s</Button>
+          <Button variant="contained">Most 4s</Button>
+          <Button variant="contained">Highest Run Scores</Button>
+          <Button variant="contained">Tour Data</Button>
+        </ButtonGroup>
+        <ButtonGroup className={classes.btnGrp}>
+          <Button variant="contained" className={classes.fullWidthHeight50}>
+            Title Header
+          </Button>
+          <Button variant="contained" className={classes.fullWidthHeight50}>
+            Title SubHeader
+          </Button>
+        </ButtonGroup>
+      </Grid>
+    </>
+  );
+};
